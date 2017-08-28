21740d7bfb9ba22ab77592f108dbe4b7eda45fa9
Alina Volodymyrivna Sedletska <asedl@softserveinc.com>
2017-08-28 12:05:34 +0300

    Code style modified

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 (function () {
-    var app = angular.module('toDo', []),
-        taskList = [
+    var taskList = [
             {
                 name: 'Task1',
                 hours: 1,
                 date: '02/01/2016'
             }, {
                 name: 'Task2',
-                hours: 2,
+                hours: 22,
                 date: '02/01/2016'
             }
         ];
 
-    app.controller('ToDoController', function () {
+    angular
+        .module('toDo', []);
+
+    angular
+        .module('toDo')
+        .controller('ToDoController', ToDoController);
+
+    function ToDoController () {
         this.tasks = taskList;
         this.newTask = {};
 
@@ -24,6 +30,6 @@
         this.clearAll = function () {
             this.tasks.length = 0;
         };
-    });
+    };
 })();
 
